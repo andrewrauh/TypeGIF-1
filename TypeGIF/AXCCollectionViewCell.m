a42f5b4a8320f60d4db8186de3695456c1651eac
Natasja Nielsen <natmnielsen@gmail.com>
2015-04-14 20:53:12 -0400

    adjustments to shake cells

--- TypeGIF/AXCCollectionViewCell.m
+++ TypeGIF/AXCCollectionViewCell.m
@@ -35,12 +35,12 @@
 - (void)shake:(BOOL)editing {
     CABasicAnimation *quiverAnim = [CABasicAnimation animationWithKeyPath:@"transform.rotation"];
     if (editing) {
-        float startAngle = (-2) * M_PI/180.0;
+        float startAngle = (-2) * M_PI/270.0;
         float stopAngle = -startAngle;
         quiverAnim.fromValue = [NSNumber numberWithFloat:startAngle];
         quiverAnim.toValue = [NSNumber numberWithFloat:3 * stopAngle];
         quiverAnim.autoreverses = YES;
-        quiverAnim.duration = 0.2;
+        quiverAnim.duration = 0.15;
         quiverAnim.repeatCount = HUGE_VALF;
         float timeOffset = (float)(arc4random() % 100)/100 - 0.50;
         quiverAnim.timeOffset = timeOffset;
