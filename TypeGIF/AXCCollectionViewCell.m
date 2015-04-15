7c7278efd4a75aa6e231aae5903f9a8dda7eaeee
Andrew Rauh <andrew@andrewrauhcom>
2015-04-14 22:10:50 -0400

    slight animation adjustments (to match app movement)

--- TypeGIF/AXCCollectionViewCell.m
+++ TypeGIF/AXCCollectionViewCell.m
@@ -41,17 +41,18 @@
 - (void)shake:(BOOL)editing {
     CABasicAnimation *quiverAnim = [CABasicAnimation animationWithKeyPath:@"transform.rotation"];
     if (editing) {
-        float startAngle = (-2) * M_PI/270.0;
+        float startAngle = (-1) * M_PI/270.0;
         float stopAngle = -startAngle;
         quiverAnim.fromValue = [NSNumber numberWithFloat:startAngle];
         quiverAnim.toValue = [NSNumber numberWithFloat:3 * stopAngle];
         quiverAnim.autoreverses = YES;
-        quiverAnim.duration = 0.2;
+        quiverAnim.duration = 0.1;
         quiverAnim.repeatCount = HUGE_VALF;
         float timeOffset = (float)(arc4random() % 100)/100 - 0.50;
         quiverAnim.timeOffset = timeOffset;
         CALayer *layer = self.layer;
         [layer addAnimation:quiverAnim forKey:@"shake"];
+        [layer setAllowsEdgeAntialiasing:YES];
         [self addSubview:self.editingView];
     } else {
         CALayer *layer = self.layer;
